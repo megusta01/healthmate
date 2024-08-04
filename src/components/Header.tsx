8a261f1ca5f0bab6feb59c893a8eba99fcc86d87
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2024-08-04 02:32:10 -0300

    Adicionando header, footer e logo.

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="header text-white p-4 text-center">
+            <img src="Healthmate-header.png" alt="" />
+        </header>
+    );
+}
