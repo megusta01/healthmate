9cd9cabc938e914e48439209e77fb8e16c71cae4
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2024-09-03 20:58:07 -0300

    Ajustando estilo do Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import styles from '../styles/Modal.module.css'; // Importa o arquivo CSS module
 
 interface ModalProps {
   isOpen: boolean;
@@ -8,36 +9,13 @@ interface ModalProps {
   setModalOpen: () => void;
 }
 
-const BACKGROUND_STYLE: React.CSSProperties = {
-  position: 'fixed',
-  top: '0',
-  bottom: '0',
-  left: '0',
-  right: '0',
-  backgroundColor: 'rgba(0, 0, 0, 0.7)',
-  zIndex: 1000,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center'
-}
-
-const MODAL_STYLE: React.CSSProperties = {
-  position: 'relative',
-  backgroundColor: 'white',
-  padding: '30px',
-  borderRadius: '20px',
-  zIndex: 1001,
-  width: '80%',
-  maxWidth: '500px'
-}
-
 const Modal: React.FC<ModalProps> = ({ isOpen, setModalOpen, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div style={BACKGROUND_STYLE} onClick={setModalOpen}>
-      <div style={MODAL_STYLE} onClick={(e) => e.stopPropagation()}>
-        <button className='absolute top-4 right-4 text-gray-500 hover:text-gray-700' onClick={setModalOpen}>
+    <div className={styles.background}>
+      <div className={styles.modal}>
+        <button className={styles.closeButton} onClick={setModalOpen}>
           <FontAwesomeIcon icon={faCircleXmark} />
         </button>
         {children}
