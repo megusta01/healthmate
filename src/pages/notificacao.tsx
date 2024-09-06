c288e869b614e8634d8eabcec725dee9496fbad6
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2024-09-06 16:33:07 -0300

    arquivo de notificacao

---
+++ src/pages/notificacao.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+export default function Notificacao() {
+    return (
+        <>
+            <Header />
+            <div>
+                <h1>Notificação</h1>
+            </div>
+        </>
+    );
+}
