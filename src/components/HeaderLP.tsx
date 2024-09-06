25f96436bf487c54f9839863712f4cebc86db098
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2024-09-06 00:22:13 -0300

    Header da landing page

---
+++ src/components/HeaderLP.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from '../styles/HeaderLP.module.css'; // Importando o arquivo CSS
+
+const Header: React.FC = () => {
+return (
+    <header className={styles.header}>
+        <div className={styles.container}>
+            {/* Logo */}
+            <div className={styles.logo}>
+                <img src="/logo (1).svg" alt="Logo" className={styles.logoImg} />
+            </div>
+
+            {/* Menu */}
+            <nav className={styles.menu}>
+                <a href="#features" className={styles.menuItem}>
+                    FUNCIONALIDADES
+                </a>
+                <a href="#benefits" className={styles.menuItem}>
+                    GUIA RÁPIDO
+                </a>
+                <a href="#about" className={styles.menuItem}>
+                    BENEFÍCIOS
+                </a>
+            </nav>
+
+            {/* Botão de Login */}
+            <button className={styles.loginBtn} onClick={() => window.location.href = "/Login"}>LOGIN</button>
+        </div>
+    </header>
+);
+};
+
+export default Header;
